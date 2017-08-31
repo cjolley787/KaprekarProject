2dd05d586e1ad96a72ea0c3b9cd163275f100e01
user <user@10.214.97.56>
2017-08-31 15:00:48 -0500

    Changed the upper value of for loop from numDigits to 2*numDigits. Sometimes the break in the Kaprekar number occurs more digits into the square than the entire original number contains. Thus doubling the ceiling allows all cases to be tested.

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -33,7 +33,7 @@ public class KaprekarUtils {
 		int numDigits = (int) Math.log10(n) + 1;
 		long modulus = 1;
 		long first, second;
-		for(int i=1; i<=numDigits; i++) {
+		for(int i=1; i<=2*numDigits; i++) {
 			modulus *= 10;
 			first = square / modulus;
 			second = square % modulus;
